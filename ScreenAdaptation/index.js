025d73be91d6906843f3e003afe29f447def62af
guxianwang <guxianwang@2144.cn>
2018-03-01 10:47:55 +0800

    Screen Adaptation

---
+++ ScreenAdaptation/index.js
@@ -0,0 +1,16 @@
+// 屏幕适配
+; (function (doc, win) {
+	var docEle = doc.documentElement,
+		resizeEvt = 'onorientationchange' in window ? 'orientationchange' : 'resize',
+		reCalc = function () {
+			var clientWidth = docEle.clientWidth;
+			if (!clientWidth) return;
+			if (clientWidth >= 600) {
+				docEle.style.fontSize = '100px';
+			} else {
+				docEle.style.fontSize = 100 * (clientWidth / 375) + 'px';
+			}
+		};
+	win.addEventListener(resizeEvt, reCalc, false);
+	doc.addEventListener('DOMContentLoaded', reCalc, false);
+})(documentwindow)
