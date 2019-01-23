a64c3128034c78cb75b09d22b933433d6b1a394e
gintangible <gintangible@163.com>
2019-01-24 00:19:28 +0800

    三级联动 begining

--- ScrollBar/index.js
+++ ScrollBar/index.js
@@ -1,9 +1,31 @@
 class ScrollBar {
     constructor(config) {
         this.el = config.el;
+        this.init();
     }
+
+    init(){
+        this.create();
+        this.move();
+    }
+
     create(){
-       
+       let el = this.el;
+       let div = document.createElement("div");
+       div.className = 'scroll-bar';
+       div.innerHTML = '<span></span>';
+       el.parentNode.append(div);
+    }
+    positon(){
+        let div = document.querySelector('scroll-bar');
+        let scrollY = this.el.scrollTop;
+        console.log(scrollY)
+    }
+    move(){
+        const self = this;
+        this.el.addEventListener('scroll',function(){
+            self.positon()
+        })
     }
 
 }
@@ -21,4 +43,4 @@ class ScrollBar {
 
 var scrollBar = new ScrollBar({
     el: document.querySelector('.scroll-wrapper')
-})
+});
