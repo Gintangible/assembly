324723a5f7e22cc0cdbd9badba9cda82c2df58bc
gintangible <gintangible@163.com>
2019-02-27 15:30:09 +0800

    upload

--- Upload/upload.js
+++ Upload/upload.js
@@ -1,19 +1,4 @@
 var updataImgFn = function () {
-    /**
-     * tailorSize           []              裁剪的大小
-     * isPreview            Boolean         是否预览
-     * view                                 展示预览图的容器
-     * multiple             Boolean         多图
-     * limitNum             number          限制的图片数
-     */
-    var defaultOption = {
-        isPreview: true,
-        view: null,
-        multiple: true,
-        imgSave: [],
-        limitNum: 2
-    };
-
     // 创建input[type="file"]
     function createInputFile(el) {
         const inputFile = document.createElement('input');
@@ -34,15 +19,6 @@ var updataImgFn = function () {
         console && console[type || "log"](name);
     }
 
-    function createPreview(el) {
-        const img = document.createElement('img');
-        const parent = el.parentNode;
-
-        parent.appendChild(img);
-        return img;
-    }
-
-
     function imgUpload(el, options) {
         const getEl = typeof el === 'string' ? document.querySelector(el) : el;
         const isInput = getEl.nodeName.toLowerCase() === 'input';
@@ -52,9 +28,26 @@ var updataImgFn = function () {
             getEl.type = 'file';
         }
 
+        /**
+         * isPreview            Boolean         是否预览
+         * view                                 展示预览图的容器
+         * multiple             Boolean         多图
+         * limitNum             number          限制的图片数
+         */
+        var defaultOption = {
+            isPreview: true,
+            view: null,
+            multiple: null,
+            imgSave: [],
+            limitNum: 1,
+            isDelete: true,
+            deleteClass: 'delele-btn',
+            viewItemClass: 'view-item'
+        };
+
         this.el = isInput ? getEl : createInputFile(getEl);
 
-        this.options = Object.assign(defaultOption, options || {})
+        this.options = Object.assign(defaultOption, options || {});
 
         this._init(this.options);
     }
@@ -66,42 +59,28 @@ var updataImgFn = function () {
             for (let k in options) {
                 this[k] = options[k];
             }
+
+            this.multiple = this.multiple || this.el.multiple;
+            this.view = typeof this.view === 'string' ? document.querySelector(this.view) : this.view ? this.view : this.el.parentNode;
         },
 
         _init: function (options) {
             this._analysisOptions(options);
-            this._getViewEl();
             this._bind();
         },
 
         // input change 
         _bind: function () {
             const self = this;
-            this.el.addEventListener('change', function () {
-                if(this.imgSave.length >= this.limitNum+1) {
-                    throw(`图片最多上传${this.limitNum}个`);
-                };
+            this.el.addEventListener('change', function () {    
                 const files = this.files;
-                if (files.length) {
-                    var reader = new FileReader();
-                    reader.onloadstart = function (e) {
-                        typeof self._loadStart === 'function' && self._loadStart(e);
-                    }
-                    reader.onprogress = function (e) {
-                        typeof self._loadProgress === 'function' && self._loadProgress(e);
-                    }
-                    reader.onabort = function (e) {
-                        msgConsole("中断读取....");
-                    }
-                    reader.onerror = function (e) {
-                        msgConsole("读取异常....");
-                    }
-                    reader.onload = function (e) {
-                        typeof self._loadComplete === 'function' && self._loadComplete(e);
-                        self._preview(this.result);
-                        self._saveImgData(files);
-                    }
-                    reader.readAsDataURL(files);
+                const len = files.length;
+                if (this.multiple && files.length > self.limitNum) {
+                    throw (`图片最多上传${self.limitNum}个`);
+                };
+                self._createPreview(len);
+                for (let i = 0; i < len; i++) {
+                    self._fileHandle(i, files[i]);
                 }
             });
             // this.el.addEventListener('click', function () {
@@ -109,26 +88,27 @@ var updataImgFn = function () {
             // })
         },
 
-        _saveImgData: function(file){
-            if(!this.multiple) this.imgSave = [];
-            this.imgSave.push({
-                file: file,
-                name: file.name
-            })
-        },
-
-        _getImgData: function(){
-            return this.imgSave;
-        },
-
-        _reset: function (callback) {
-            // file 文件清空
-            this.el.value = '';
-            // 预览图片清空
-            this.view.querySelector('img').src = '';
-            // 保存图片地址清空
-            this.imgSave = [];
-            typeof callback === 'function' && callback();
+        _fileHandle: function (i, file) {
+            const self = this;
+            var reader = new FileReader();
+            reader.onloadstart = function (e) {
+                typeof self._loadStart === 'function' && self._loadStart(e);
+            }
+            reader.onprogress = function (e) {
+                typeof self._loadProgress === 'function' && self._loadProgress(e);
+            }
+            reader.onabort = function (e) {
+                msgConsole("中断读取....");
+            }
+            reader.onerror = function (e) {
+                msgConsole("读取异常....");
+            }
+            reader.onload = function (e) {
+                typeof self._loadComplete === 'function' && self._loadComplete(e);
+                self._saveImgData(i, file, this.result);
+                self._preview(i, this.result);
+            }
+            reader.readAsDataURL(file);
         },
 
         _loadComplete: function (e) {
@@ -144,36 +124,67 @@ var updataImgFn = function () {
             msgConsole(`正在读取中....`);
         },
 
-        // 预览
-        _preview: function (src) {
-            if (!this.isPreview) return;
+        _saveImgData: function (i, file, result) {
+            if (!this.multiple) this.imgSave = [];
+            this.imgSave[i] = {
+                file: file,
+                name: file.name,
+                result: result
+            };
+        },
 
-            const img = this.view.querySelector('img');
-            if (!img) {
-                img = document.createElement('img');
-                this.view.appendChild(img);
+        _getImgData: function () {
+            return this.imgSave;
+        },
+
+        _reset: function (callback) {
+            // file 文件清空
+            this.el.value = '';
+            // 预览图片清空
+            this.view.innerHTML = '';
+            // 保存图片地址清空
+            this.imgSave = [];
+            typeof callback === 'function' && callback();
+        },
+
+        _createPreview: function(totalLen){
+            if (!this.isPreview) return;
+            this.view.innerHTML = '';
+            const img = this.view.querySelectorAll('img');
+            let len = img.length;
+
+            while(len < totalLen){
+                let createImg = document.createElement('img');
+                this.view.appendChild(createImg);
+                len++;
             }
+        },
+        // 预览
+        _preview: function (i, src) {
+            /**
+             * <div> view
+             *      <img>
+             * </div>
+             */
+            // 加载的图片顺序不一样
+            if (!this.isPreview) return;
+            let img = this.view.querySelectorAll('img')[i];
 
             img.src = src;
-        },
-
-        _getViewEl: function () {
-            // 仅预览的时候，大部分input 和img在用一个位置
-            if (!this.isPreview) return;
-            const view = this.view || createPreview(this.el);
-            const isImg = view.nodeName.toLowerCase() === 'img';
-            this.view = isImg ? view.parentNode : view;
-        },
-    }
-
-    window.onload = function () {
-        var files = document.querySelectorAll('input[type="file"');
-        files.forEach(function (item) {
-            new imgUpload(item, {})
-        })
+        }
     }
 
     return {
         imgUpload: imgUpload
     }
 }();
+
+window.onload = function () {
+    var files = document.querySelectorAll('input[type="file"');
+    files.forEach(function (item, i) {
+        var a = 'file' + i;
+        window[a] = new updataImgFn.imgUpload(item, {
+            limitNum: 3
+        })
+    })
+}
