4d0b6f00046d4d8df820dc94d3fd2630ff314319
gintangible <gintangible@163.com>
2019-03-25 21:13:55 +0800

    updata pagenation

--- pagenation2/index.js
+++ pagenation2/index.js
@@ -11,22 +11,42 @@
          * cur           string  按钮激活类名
          */
         let baseOptions = Object.assign({
-            arrow: true,
-            data: '',
-            showDataCount: 1,
-            showLen: 10,
-            cur: 'cur'
+            data: [],
+            showDataCount: 10,
+            curPage: 0,
+            isArrow: true,
+            showNum: 5,
+            prefix: 'page-',
+            disabledClass: 'page-disabled',
+            isTail: true,
+            pageDetail: true,
+            isShowInfo: false,
+            ellipsisClass: ''
         }, options);
 
         this.el = el;
         this.dataAry = [];
-        this.curIndex = 0;
+
+        /*
+         * prev     string      上一页
+         * next     string      下一页
+         * start    string      首页
+         * end      string      尾页
+         * page     strign      具体的页码
+         * info     string      页码详情
+         */
+        this.prevHTML = '';
+        this.nextHTML = '';
+        this.startHTML = '';
+        this.endHTML = '';
+        this.pageHTML = '';
+        this.infoHTML = '';
 
         this._init(baseOptions);
     };
 
     Paganation.prototype = {
-        contructor: Paganation,
+        constructor: Paganation,
 
         _analysisOptions: function (options) {
             for (let k in options) {
@@ -35,7 +55,7 @@
         },
 
         _init: function (options) {
-            const _this = this;
+            var _this = this;
             this._analysisOptions(options);
 
             this._dataParse(this.data, function () {
@@ -44,9 +64,9 @@
         },
 
         _dataParse: function (data, callback) {
-            const showDataCount = this.showDataCount;
-            const pageCount = Math.ceil(data.length / showDataCount);
-            for (let i = 0; i < pageCount; i++) {
+            var showDataCount = this.showDataCount;
+            var pageCount = this.totalPage = Math.ceil(data.length / showDataCount);
+            for (var i = 0; i < pageCount; i++) {
                 this.dataAry.push({
                     page: i,
                     data: data.slice(showDataCount * i, showDataCount * (i + 1))
@@ -60,8 +80,8 @@
         _render: function (index) {
             // index 为当前页
             this.curPage = +index;
-            this._pageRender(index);
             this._contentRender(index);
+            this._pageRender();
         },
 
         _contentRender: function (index) {
@@ -69,66 +89,114 @@
                 node = item.element;
             if (!node) {
                 if (typeof this._contentFormat !== 'function') {
-                    throw new Error(`_contentFormat必须是一个function`);
+                    throw new Error('_contentFormat必须是一个function');
                 }
                 node = this._contentFormat(item.data);
                 item.element = node;
             }
-
             if (typeof this._contentShow !== 'function') {
-                throw new Error(`_contentShow必须是一个function`);
+                throw new Error('_contentShow必须是一个function');
             }
 
             this._contentShow(node);
         },
         _contentFormat: function (data) {
-            let str = '';
+            var str = '';
             data.forEach((item, index) => {
                 str += '<div>' + item + '</div>'
             })
             return str;
         },
 
-        _contentShow: function (node) {
+        _contentShow: function (node, content, callback) {
             // 此处为内容渲染的函数
-            console.log(`you must set you _contentShow function`);
-            doc.querySelector('.page-content').innerHTML = node;
-        },
-
-        _pageHtmlStr: function (i) {
-            const isCur = i === this.curPage ? this.cur : '';
-            return `<span data-page="${i}" class="${isCur}">${(i + 1)}</span>`;
-        },
-
-        _pageRender: function (index) {
-            let htmlStr = '';
-            const showLen = this.showLen;
-            const totalPage = this.dataAry.length;
-            const curPage = index;
-            const centerStr = `<span>...</span>`;
-            if (showLen >= totalPage) {
-                for (let i = 0; i < totalPage; i++) {
-                    htmlStr += this._pageHtmlStr(i);
-                }
-            } else {
-                if (curPage <= totalPage - showLen) { //头部
-                    for (let i = 0; i < showLen - 1; i++) {
-                        htmlStr += this._pageHtmlStr(+curPage + i);
-                    }
-                    htmlStr += centerStr + this._pageHtmlStr(totalPage - 1);
-                } else { // 尾部
-                    for (let i = 0; i < showLen; i++) {
-                        htmlStr += this._pageHtmlStr(totalPage - showLen + i);
-                    }
-                }
-                if (this.arrow) {
-                    const prev = `<span data-page="${(curPage <= 0 ? 0 : (curPage - 1))}">上一页</span>`;
-                    const next = `<span data-page="${curPage >= totalPage - 1 ? totalPage - 1 : (+curPage + 1)}">下一页</span>`;
-                    htmlStr = prev + htmlStr + next;
-                };
+            console.log('you must set you _contentShow function');
+            var content = content || doc.querySelector('.page-content');
+            if (content) {
+                content.innerHTML = node;
             }
 
-            this.el.innerHTML = htmlStr;
+            callback && callback();
+        },
+
+        // set html
+        _create: function () {
+            this.curPage = +this.curPage;
+            this.totalPage = +this.totalPage;
+            if (this.curPage < 0) this.curPage = 0;
+            if (this.curPage > this.totalPage - 1) this.curPage = this.totalPage - 1;
+            if (this.isArrow) this._createArrow();
+            if (this.isTail) this._createTail();
+            if (this.isShowInfo) this._createInfo();
+            if (this.pageDetail) this._createPage();
+        },
+
+        // 上下页
+        _createArrow: function () {
+            const disabledClass = this.disabledClass;
+            const prevPage = this.curPage - 1;
+            const nextPage = +this.curPage + 1;
+            const prevDisabled = prevPage < 0 ? disabledClass : '';
+            const nextDisabled = nextPage > this.totalPage - 1 ? disabledClass : '';
+
+            this.prevHTML = '<span class="' + this.prefix + 'item ' + prevDisabled + '" data-page="' + prevPage + '">上一页</span>';
+            this.nextHTML = '<span class="' + this.prefix + 'item ' + nextDisabled + '" data-page="' + nextPage + '">下一页</span>';
+        },
+
+        // 首尾页
+        _createTail: function () {
+            const disabledClass = this.disabledClass;
+            const startPage = 0;
+            const endPage = this.totalPage - 1;
+            const startDisabled = this.curPage == 0 ? disabledClass : '';
+            const endDisabled = this.curPage == this.totalPage - 1 ? disabledClass : '';
+            this.startHTML = '<span class="' + this.prefix + 'item ' + startDisabled + '" data-page="' + startPage + '">首页</span>';
+            this.endHTML = '<span class="' + this.prefix + 'item ' + endDisabled + '" data-page="' + endPage + '">尾页</span>';
+        },
+
+        // 页数详情
+        _createInfo: function () {
+            this.infoHTML = '<span class="' + this.prefix + 'total">共<i>' + this.totalPage + '</i>页</span><span class="' + this.prefix + 'cur">当前第<i>' + this.curPage + '</i>页</span>'
+        },
+
+        // 具体的页码
+        _createPage: function () {
+            let str = '';
+            let self = this;
+            let showNum = this.showNum;
+            let totalPage = this.totalPage;
+            let curPage = this.curPage;
+            const prefix = this.prefix;
+            const centerStr = '<span class="' + this.ellipsisClass + '">...</span>';
+
+            let htmlStr = function (i) {
+                const endabled = self.curPage == i ? self.disabledClass : '';
+                return '<span data-page="' + i + '" class="' + endabled + '">' + (i + 1) + '</span>';
+            };
+
+            if (showNum >= totalPage) {
+                for (let i = 0; i < totalPage; i++) {
+                    str += htmlStr(i)
+                }
+            } else {
+                if (curPage < totalPage - showNum + 1) { //头部
+                    for (let i = 0; i < showNum - 1; i++) {
+                        str += htmlStr(+curPage + i);
+                    }
+                    str += centerStr + htmlStr(totalPage - 1);
+                } else { // 尾部
+                    for (let i = 0; i < showNum; i++) {
+                        str += htmlStr(totalPage - showNum + i);
+                    }
+                }
+            }
+
+            this.pageHTML = str;
+        },
+
+        _pageRender: function () {
+            this._create();
+            this.el.innerHTML = this.infoHTML + this.startHTML + this.prevHTML + this.pageHTML + this.nextHTML + this.endHTML;
             this._bindEvent();
         },
 
@@ -139,6 +207,7 @@
             spans.forEach(item => {
                 item.addEventListener('click', function () {
                     const index = this.getAttribute('data-page');
+                    if (this.className.indexOf(_this.disabledClass) > -1) return;
                     _this._refresh(index);
                 });
             });
@@ -152,8 +221,48 @@
     window.Paganation = Paganation;
 })(document);
 
-// 根据接口数据在分页
+
+var parseURL = function () {
+    var cache = {};
+    return function (url) {
+        if (!cache[url]) {
+            var a = document.createElement('a');
+            a.href = url ? url : window.location.href;
+            cache[url] = {
+                source: url,
+                protocol: a.protocol.replace(':', ''),
+                host: a.hostname,
+                port: a.port,
+                query: a.search,
+                params: (function () {
+                    var ret = {},
+                        seg = a.search.replace(/^\?/, '').split('&'),
+                        len = seg.length,
+                        i = 0,
+                        s;
+                    for (; i < len; i++) {
+                        if (!seg[i]) {
+                            continue;
+                        }
+                        s = seg[i].split('=');
+                        ret[s[0]] = s[1];
+                    }
+                    return ret;
+                })(),
+                file: (a.pathname.match(/\/([^\/?#]+)$/i) || [, ''])[1],
+                hash: a.hash.replace('#', ''),
+                path: a.pathname.replace(/^([^\/])/, '/$1'),
+                relative: (a.href.match(/tps?:\/\/[^\/]+(.+)/) || [, ''])[1],
+                segments: a.pathname.replace(/^\//, '').split('/')
+            };
+        }
+        return cache[url];
+    };
+}();
+
+// 显示数目
 new Paganation(document.querySelector('.page-container'), {
-    data: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
-    showDataCount: 1
+    baseUrl: "?page=",
+    curPage: parseURL().params.page,
+    totalPage: 100
 })
