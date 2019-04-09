cee9cd55503dac27d528caf4df5419f5448131a5
gintangible <gintangible@163.com>
2019-04-09 14:54:35 +0800

    easy countDown

--- CountDown/countdown.js
+++ CountDown/countdown.js
@@ -1,57 +1,5 @@
 ;
 (function () {
-    function Timer(delay) {
-        this._queue = [];
-        this.stop = false;
-        this._createTimer(delay);
-    }
-
-    Timer.prototype = {
-        construct: Timer,
-        _createTimer: function (delay) {
-            var self = this;
-            var first = true;
-            (function () {
-                var s = new Date();
-                self._queue.forEach(function (item) {
-                    item();
-                })
-                if (!self.stop) {
-                    var cost = new Date() - s;
-                    delay = first ? delay : ((cost > delay) ? cost - delay : delay);
-                    setTimeout(arguments.callee, delay);
-                }
-            })();
-            first = false;
-        },
-        add: function (cb) {
-            this._queue.push(cb);
-            this.stop = false;
-        },
-        remove: function (index) {
-            this._queue.splice(index, 1);
-            if (!this._queue.length) {
-                this.stop = true;
-            }
-        }
-    }
-
-    function TimePool() {
-        this._pool = {};
-    }
-    TimePool.prototype = {
-        construct: TimePool,
-        getTimer: function (delayTime) {
-            var t = this._pool[delayTime];
-            return t ? t : (this._pool[delayTime] = new Timer(delayTime));
-        },
-        removeTimer: function (delayTime) {
-            if (this._pool[delayTime]) {
-                delete this._pool[delayTime];
-            }
-        }
-    }
-
     function leftPad(str, len, ch) {
         str = String(str);
         var i = -1;
@@ -64,19 +12,22 @@
     }
 
     function formatTime(ms) {
-        var s = ms / 1000,
-            m = s / 60;
+        var s = Math.floor(ms / 1000),
+            m = Math.floor(s / 60);
+        console.log(leftPad(m / 60 / 24, 2, 0), leftPad(m / 60 % 24, 2, 0), leftPad(m % 60, 2, 0), leftPad(s % 60, 2, 0))
         return {
             d: leftPad(m / 60 / 24, 2, 0),
             h: leftPad(m / 60 % 24, 2, 0),
-            d: leftPad(m % 60, 2, 0),
-        }
+            m: leftPad(m % 60, 2, 0),
+            s: leftPad(s % 60, 2, 0)
+        };
     }
 
     function CountDown(config = {}) {
         var defaultOptions = {
-            fixNow: 3 * 1000,
-            fixNowDate: false,
+            delaytime: 1000,
+            template: '{d}:{h}:{m}:{s}',
+            now: new Date().valueOf(),
             render: function (outstring) {
                 console.log(outstring);
             },
@@ -99,44 +50,31 @@
         construct: CountDown,
         init: function () {
             var self = this;
-            if (this.fixNowDate) {
-                var fix = new timer(this.fixNow);
-                fix.add(function () {
-                    self.getNowTime(function (now) {
-                        self.now = now;
-                    });
-                });
-            }
-            var index = msInterval.add(function () {
-                self.now += delayTime;
+            (function () {
+                self.now += self.delaytime
                 if (self.now >= self.endTime) {
-                    msInterval.remove(index);
                     self.end();
                 } else {
+                    setTimeout(arguments.callee, self.delaytime);
                     self.render(self.getOutString());
                 }
-            });
+            })();
         },
         getBetween: function () {
-            return _formatTime(this.endTime - this.now);
+            return formatTime(this.endTime - this.now);
         },
         getOutString: function () {
             var between = this.getBetween();
+            console.log(between)
             return this.template.replace(/{(\w*)}/g, function (m, key) {
                 return between.hasOwnProperty(key) ? between[key] : "";
             });
-        },
-        getNowTime: function (cb) {
-            var xhr = new XMLHttpRequest();
-            xhr.open('get', '/', true);
-            xhr.onreadystatechange = function () {
-                if (xhr.readyState === 3) {
-                    var now = xhr.getResponseHeader('Date');
-                    cb(new Date(now).valueOf());
-                    xhr.abort();
-                }
-            };
-            xhr.send(null);
         }
     }
+
+    var now = Date.now();
+
+    new CountDown({
+        endTime: now + 5 * 1000
+    });
 })();
