bf0d767e4fdd34120728b504a39b064d3d8880ba
gintangible <gintangible@163.com>
2019-05-16 17:14:00 +0800

    updata

---
+++ window/index.js
@@ -0,0 +1,24 @@
+window.onerror = function(message, url, line, column, error) {
+    console.log('log---onerror::::', message, url, line, column, error);
+}
+
+
+function ping(options) {
+    const nIMG = new Image();
+    const DEFAULTOPTIONS = {
+        url: location.origin,
+        afterPing: function(ping) {
+            console.log(ping);
+        },
+        interval: 1000
+    }
+
+    const obj = Object.assign(DEFAULTOPTIONS, options);
+    const url = (/http:\/\//.test(obj.url)) ? obj.url : 'http' + obj.url;
+    const startTime = +new Date();
+    nIMG.onload = nIMG.onerror = function() {
+        const endTime = +new Date();
+        obj.afterPing(endTime - startTime);
+    }
+    nIMG.src = url + '/' + startTime;
+}
