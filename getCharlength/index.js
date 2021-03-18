f00114e5529e112be98b65fd9b5bc89b9687053e
guxianwang <guxw@njzhyl.cn>
2021-03-18 18:06:08 +0800

    this will be deleted

--- getCharlength/index.js
+++
@@ -1,7 +0,0 @@
-function getCharLength(str) {
-    return String(str).trim().split('')
-        .map(s => s.charCodeAt())
-        .map(n => (n < 0 || n > 255) ? 'aa' : 'a') // 中文占两字符, 其他一字符
-        .join('')
-        .length;
-};
--- windowError/index.js
+++
@@ -1,4 +0,0 @@
-console.log(123123);
-window.onerror = function(message, url, line, column, error) {
-    console.log('log---onerror::::', message, url, line, column, error);
-}
